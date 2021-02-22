1359abee7af657d756208575b97f46efb7428bfc
danikleonov <47587031+danikleonov@users.noreply.github.com>
2021-02-22 10:18:16 +0200

    Race

---
+++ src/by/gsu/pms/Race.java
@@ -0,0 +1,42 @@
+package by.gsu.pms;
+
+public class Race {
+    private String data;
+    private int numberRiders;
+    private String prize;
+
+    public Race() { super(); }
+
+    public Race(String data, int numberRiders, String prize) {
+        this.data = data;
+        this.numberRiders = numberRiders;
+        this.prize = prize;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public int getNumberRiders() {
+        return numberRiders;
+    }
+
+    public void setNumberRiders(int numberRiders) {
+        this.numberRiders = numberRiders;
+    }
+
+    public String getPrize() {
+        return prize;
+    }
+
+    public void setPrize(String prize) {
+        this.prize = prize;
+    }
+
+    
+
+}
