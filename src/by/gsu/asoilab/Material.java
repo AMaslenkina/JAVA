d3d3dffd725ddb4fd0574875a07c7a023f8b985c
MaslenkinaAnastasiya <nastya2003380@gmail.com>
2021-03-04 10:55:20 +0200

    feat: add class material

---
+++ src/by/gsu/asoilab/Material.java
@@ -0,0 +1,13 @@
+package by.gsu.asoilab;
+
+public class Material {
+    private String material;
+    private int volume;
+
+    Density name1 = new Density();
+    Density name2;
+
+    public Material(){
+        super();
+    }
+}
