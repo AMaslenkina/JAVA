3d95326a57a4e53beb69e2bd592d7f0ffd3d42e6
AMaslenkina <43391778+AMaslenkina@users.noreply.github.com>
2021-03-21 22:44:08 +0200

    feat: add output

--- src/by/gsu/asoilab/Runner.java
+++ src/by/gsu/asoilab/Runner.java
@@ -2,6 +2,10 @@ package by.gsu.asoilab;
 
 public class Runner {
     public static void main(String[] args){
+        Density steel = new Density("Steel", 7850);
+        Density copper = new Density("Copper", 8500);
 
+        System.out.println(steel.toString());
+        System.out.println(copper.toString());
     }
 }
