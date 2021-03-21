684a0b2f859bd3a8f8a96f17a84a20609b193a78
AMaslenkina <43391778+AMaslenkina@users.noreply.github.com>
2021-03-21 22:42:24 +0200

    feat:  add materials getter and setter

--- src/by/gsu/asoilab/Material.java
+++ src/by/gsu/asoilab/Material.java
@@ -1,13 +1,46 @@
 package by.gsu.asoilab;
 
 public class Material {
-    private String material;
+    private String name;
     private int volume;
+    private Density density;
 
-    Density name1 = new Density();
-    Density name2;
+    public Material(String name, double volume, Density density) {
+        this.name = name;
+        this.volume = volume;
+        this.density = density;
+    }
 
-    public Material(){
-        super();
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+    public void setVolume(double volume) {
+        this.volume = volume;
+    }
+
+    public Density getDensity() {
+        return density;
+    }
+
+    public void setDensity(Density density) {
+        this.density = density;
+    }
+
+    public double getMass() {
+        return density.getDensity()*this.volume;
+    }
+
+    @Override
+    public String toString() {
+        return name + ";" + density + ";" + volume + ";" + getMass();
     }
 }
