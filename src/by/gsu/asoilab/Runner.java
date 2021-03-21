f097ac69b64ab21a97408d1c239d29665e3a013c
AMaslenkina <43391778+AMaslenkina@users.noreply.github.com>
2021-03-21 23:02:23 +0200

    feat: add wire output

--- src/by/gsu/asoilab/Runner.java
+++ src/by/gsu/asoilab/Runner.java
@@ -4,8 +4,10 @@ public class Runner {
     public static void main(String[] args){
         Density steel = new Density("Steel", 7850);
         Density copper = new Density("Copper", 8500);
+        Material wire = new Material("Wire", 0.3,steel);
 
         System.out.println(steel.toString());
         System.out.println(copper.toString());
+        System.out.println(wire.toString());
     }
 }
