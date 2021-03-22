d88dfb30096c88547cc7a88ec5cdc122a6d5484f
MaslenkinaAnastasiya <nastya2003380@gmail.com>
2021-03-22 10:23:31 +0200

    feat: add constructor

--- task1/src/by/gsu/pms/Purchase.java
+++ task1/src/by/gsu/pms/Purchase.java
@@ -1,5 +1,13 @@
 package by.gsu.pms;
 
 public class Purchase {
+    private String name;
+    private double price;
+    private int number;
 
+    public Purchase(String name, double price, int number) {
+        this.name = name;
+        this.price = price;
+        this.number = number;
+    }
 }
