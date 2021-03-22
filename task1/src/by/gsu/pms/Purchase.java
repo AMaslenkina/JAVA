bfe87de690dd383e72e67f99e78cf2fd8d47fd4d
MaslenkinaAnastasiya <nastya2003380@gmail.com>
2021-03-22 10:27:44 +0200

    feat: add getters/setters

--- task1/src/by/gsu/pms/Purchase.java
+++ task1/src/by/gsu/pms/Purchase.java
@@ -10,4 +10,29 @@ public class Purchase {
         this.price = price;
         this.number = number;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
 }
