eaa8b3a08a8455e381d7270d4dd268cc8fd4907d
AMaslenkina <nastya2003380@gmail.com>
2021-04-26 19:22:09 +0200

    init: start pr2_3

---
+++ untitled3/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+J
+src/by/gsu/pms/Runner.java,e\e\ee718f6a2068ad84a01802c28253f6ea0abf3df3
---
+++ untitled3/src/by/gsu/pms/Runner.java
@@ -0,0 +1,8 @@
+package by.gsu.pms;
+
+public class Runner {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
