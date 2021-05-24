c8823a5df12b69de5e317abb2ad4cd4767eebe5c
AMaslenkina <nastya2003380@gmail.com>
2021-05-24 10:29:46 +0300

    feat: end task5

---
+++ src/by/gsu/pms/Serializer.java
@@ -0,0 +1,13 @@
+package by.gsu.pms;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+public class Serializer {
+    private Serializer(){}
+    public static void serialize(Race[] Races, String file) {
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(file))) {
+            outputStream.writeObject(Races);
+        } catch (Exception e) {
+        }
+    }
+}
